134eb214f56b92a605da4bb2cf0c0beba0428bf2
Kevin Gustafsson <kevin.gustafsson@combitech.se>
2020-05-22 10:12:20 +0200

    Added VPN and FW check

--- .i3/scripts/office-monitors-enable.sh
+++ .i3/scripts/office-monitors-enable.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
+xrandr --auto
 sleep 2
-xrandr --output DP-1-2 --auto
-sleep 1
-xrandr --output HDMI-1 --auto
-sleep 1
-xrandr --output HDMI-1 --right-of eDP-1
-sleep 1
-xrandr --output DP-1-2 --right-of HDMI-1
+xrandr --output DP-1-2 --right-of DP-1-1
+sleep 2
+xrandr --output DP-1-1 --right-of eDP-1
 
